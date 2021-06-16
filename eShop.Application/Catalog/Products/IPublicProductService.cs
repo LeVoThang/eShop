c9e1a4c6b5c07ba7941a4aab9158b4abfcf45959
Le Vo Thang <levothang.99@gmail.com>
2021-06-16 15:26:04 +0700

    Service layer

---
+++ eShop.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,13 @@
+﻿using eShop.Application.Catalog.Dtos;
+using eShop.Application.Catalog.Products.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Application.Catalog.Products
+{
+    public interface IPublicProductService
+    {
+        PagedViewModel<ProductViewModel> GetAllByCategoryId(int categoryId, int pageIndex, int pageSize);
+    }
+}
