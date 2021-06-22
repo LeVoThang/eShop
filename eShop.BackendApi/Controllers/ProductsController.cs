d128c84b11923ee70fa2c1cc55e55cd70c9b9545
Le Vo Thang <levothang.99@gmail.com>
2021-06-22 14:13:03 +0700

    swagger authorization

--- eShop.BackendApi/Controllers/ProductsController.cs
+++ eShop.BackendApi/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using eShop.Application.Catalog.Products;
 using eShop.ViewModels.Catalog.ProductImages;
 using eShop.ViewModels.Catalog.Products;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,6 +13,7 @@ namespace eShop.BackendApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ProductsController : ControllerBase
     {
         private readonly IPublicProductService _publicProductService;
