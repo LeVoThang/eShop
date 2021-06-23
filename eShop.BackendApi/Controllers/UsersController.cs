3e3edcac0eeeef5dddfaf93ae54ff116916746fe
Le Vo Thang <levothang.99@gmail.com>
2021-06-23 09:17:21 +0700

    cookies authentication

--- eShop.BackendApi/Controllers/UsersController.cs
+++ eShop.BackendApi/Controllers/UsersController.cs
@@ -23,20 +23,20 @@ namespace eShop.BackendApi.Controllers
 
         [HttpPost("authenticate")]
         [AllowAnonymous]
-        public async Task<IActionResult> Authenticate([FromForm] LoginRequest request)
+        public async Task<IActionResult> Authenticate([FromBody] LoginRequest request)
         {
             var resultToken = await _userService.Authenticate(request);
             if (string.IsNullOrEmpty(resultToken))
             {
                 return BadRequest("Username or password is incorrect.");
             }
-            return Ok(new { token = resultToken });
+            return Ok(resultToken);
         }
 
 
         [HttpPost("register")]
         [AllowAnonymous]
-        public async Task<IActionResult> Register([FromForm] RegisterRequest request)
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
