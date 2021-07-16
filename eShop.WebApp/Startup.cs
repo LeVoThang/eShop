50e9bb1893406b969d1bc70a633d43ddae0b907e
Thang Vo <thangvo.99@saporta.com>
2021-07-16 15:41:22 +0700

    register user

--- eShop.WebApp/Startup.cs
+++ eShop.WebApp/Startup.cs
@@ -4,7 +4,9 @@ using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using eShop.ApiIntegration;
+using eShop.ViewModels.System.Users;
 using eShop.WebApp.LocalizationResources;
+using FluentValidation.AspNetCore;
 using LazZiya.ExpressLocalization;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
@@ -38,6 +40,7 @@ namespace eShop.WebApp
             };
 
             services.AddControllersWithViews()
+                 .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<LoginRequestValidator>())
                 .AddExpressLocalization<ExpressLocalizationResource, ViewLocalizationResource>(ops =>
                 {
                     // When using all the culture providers, the localization process will
